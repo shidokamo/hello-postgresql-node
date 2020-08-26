72d5a58ffeee09524541831a09ec983b2d2bd46b
Kamome Shido <shidokamo@users.noreply.github.com>
2020-08-26 21:41:34 +0000

    ちょっとした間違いを修正

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-import { logger } from 'logger';
-import { db } from 'db';
+import { logger } from './logger';
+import db from './db';
 
 logger.info("Sent Select * query");
 db.query('SELECT * from id_and_texts WHERE id = $1', [2], (err, res) => {
   if (err) {
     logger.error("Error!");
+    logger.error(err);
+    return;
   }
   logger.info("Data" + res.rows[0])
 });
