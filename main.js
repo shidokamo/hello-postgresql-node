936e83b363ae92313a1b3fa6288e79c4f2ea4b7d
Kamome Shido <shidokamo@users.noreply.github.com>
2020-08-26 21:37:00 +0000

    とりあえずメイン関数を追加してみる

---
+++ main.js
@@ -0,0 +1,10 @@
+import { logger } from 'logger';
+import { db } from 'db';
+
+logger.info("Sent Select * query");
+db.query('SELECT * from id_and_texts WHERE id = $1', [2], (err, res) => {
+  if (err) {
+    logger.error("Error!");
+  }
+  logger.info("Data" + res.rows[0])
+});
