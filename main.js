5dbd37b927b26bd37b32c3e840057c38e5e3cf52
Kamome Shido <shidokamo@users.noreply.github.com>
2020-08-27 18:07:50 +0000

    getClient メソッドを使ってみる

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { logger } from './logger';
-import { query } from './db';
+import { query, getClient } from './db';
 
-logger.info("Sent Select * query");
+logger.info("Send query directly to a pool");
 query('SELECT * from id_and_texts WHERE id = $1', [2], (err, res) => {
   if (err) {
     logger.error("Error!");
@@ -11,3 +11,28 @@ query('SELECT * from id_and_texts WHERE id = $1', [2], (err, res) => {
   logger.info("Data")
   logger.info(JSON.stringify(res.rows[0]))
 });
+
+let i = 2;
+getClient((err, client, release) => {
+  client.query('SELECT * from id_and_texts WHERE id = $1', [i], (err, res) => {
+    if (err) {
+      logger.error("Error!");
+      logger.error(err);
+      return;
+    }
+    logger.info("Data")
+    logger.info(JSON.stringify(res.rows[0]))
+  })
+});
+//for (let i = 0; i < 1000; i++) {
+//  getClient().then(
+//    client => client.query('SELECT * from id_and_texts WHERE id = $1', [i], (err, res) => {
+//      if (err) {
+//        logger.error("Error!");
+//        logger.error(err);
+//        return;
+//      }
+//      logger.info(JSON.stringify(res.rows[0]))
+//    })
+//  );
+//}
