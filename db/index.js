529d6fdcbd68c8c8826d4da81854f8c8723a9cbb
Kamome Shido <shidokamo@users.noreply.github.com>
2020-08-27 02:06:19 +0000

    DBのクエリ発行時にログをする

--- db/index.js
+++ db/index.js
@@ -1,8 +1,14 @@
+import { logger } from '../logger'
 import { Pool } from 'pg'
 const pool = new Pool()
 
 const query = (text, params, callback) => {
-  return pool.query(text, params, callback)
+  const start = Date.now()
+  return pool.query(text, params, (err, res) => {
+     const duration = Date.now() - start
+     logger.debug('executed query', { text, duration, rows: res.rowCount })
+      callback(err, res)
+  })
 };
 
 export { query };
