bf5ac24473af505d206644fd08061452be7136d1
Kamome Shido <shidokamo@users.noreply.github.com>
2020-08-27 01:57:17 +0000

    ES6で書き直す

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
-const { Pool } = require('pg')
+import { Pool } from 'pg'
 const pool = new Pool()
 
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-}
+const query = (text, params, callback) => {
+  return pool.query(text, params, callback)
+};
+
+export { query };
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { logger } from './logger';
-import db from './db';
+import { query } from './db';
 
 logger.info("Sent Select * query");
-db.query('SELECT * from id_and_texts WHERE id = $1', [2], (err, res) => {
+query('SELECT * from id_and_texts WHERE id = $1', [2], (err, res) => {
   if (err) {
     logger.error("Error!");
     logger.error(err);
