78e3b24238e8dbbf331e1d4a956151c45f2cf650
Kamome Shido <shidokamo@users.noreply.github.com>
2020-08-27 18:10:20 +0000

    クライアントをたくさん取得する。releaseされないエラーが起きた

--- main.js
+++ main.js
@@ -12,27 +12,16 @@ query('SELECT * from id_and_texts WHERE id = $1', [2], (err, res) => {
   logger.info(JSON.stringify(res.rows[0]))
 });
 
-let i = 2;
-getClient((err, client, release) => {
-  client.query('SELECT * from id_and_texts WHERE id = $1', [i], (err, res) => {
-    if (err) {
-      logger.error("Error!");
-      logger.error(err);
-      return;
-    }
-    logger.info("Data")
-    logger.info(JSON.stringify(res.rows[0]))
-  })
-});
-//for (let i = 0; i < 1000; i++) {
-//  getClient().then(
-//    client => client.query('SELECT * from id_and_texts WHERE id = $1', [i], (err, res) => {
-//      if (err) {
-//        logger.error("Error!");
-//        logger.error(err);
-//        return;
-//      }
-//      logger.info(JSON.stringify(res.rows[0]))
-//    })
-//  );
-//}
+for (let i = 0; i < 1000; i++) {
+  // Try to check out many clients.
+  getClient((err, client, release) => {
+    client.query('SELECT * from id_and_texts WHERE id = $1', [i], (err, res) => {
+      if (err) {
+        logger.error("Error!");
+        logger.error(err);
+        return;
+      }
+      logger.info(JSON.stringify(res.rows[0]))
+    })
+  });
+}
