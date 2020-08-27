28a6a6ca75faf5ffdc07c37cafba06ea8b3703bf
Kamome Shido <shidokamo@users.noreply.github.com>
2020-08-27 02:03:30 +0000

    ログの場所の間違いを修正

--- logger/index.js
+++ logger/index.js
@@ -11,13 +11,13 @@ log4js.configure({
     },
     debug: {
       type: 'dateFile',
-      filename: '../debug/app.log',
+      filename: './debug/app.log',
       daysToKeep: 30,
       pattern: 'yyyy-MM-dd-hh',
     },
     json: {
       type: 'dateFile',
-      filename: '../json/app.log',
+      filename: './json/app.log',
       daysToKeep: 30,
       pattern: 'yyyy-MM-dd-hh',
       layout: {type: 'json', separator: ','},
